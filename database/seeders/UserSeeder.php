c16b4c002c8720e8163b919ad6ecf7b2fdfbbb4c
Germans Joels Stekels <steq112233@gmail.com>
2026-06-11 11:42:04 +0300

    feat[auth]: add admin and user seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       User::updateOrCreate(
+        ['email' => 'admin@example.com'],
+        [
+            'name' => 'Admin',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]
+       );
+
+       User::updateOrCreate(
+        ['email' => 'user@example.com'],
+        [
+            'name' => 'User',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+        ]
+       );
+    }
+
+}
